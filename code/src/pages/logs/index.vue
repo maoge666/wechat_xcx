df260f2e4778b3a8d79fb19b52f189cb24353960
maoge <673402620@qq.com>
2018-06-28 15:49:58 +0800

    上传mpvue搭建的框架

---
+++ code/src/pages/logs/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+import { formatTime } from '@/utils/index'
+
+export default {
+
+  data () {
+    return {
+      logs: []
+    }
+  },
+
+  created () {
+    const logs = (wx.getStorageSync('logs') || [])
+    this.logs = logs.map(log => formatTime(new Date(log)))
+  }
+}
+</script>
+
+<style>
+.log-list {
+  display: flex;
+  flex-direction: column;
+  padding: 40rpx;
+}
+
+.log-item {
+  margin: 10rpx;
+}
+</style>
